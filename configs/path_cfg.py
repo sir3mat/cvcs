37f5062b4c887fecf05e8ec078d6162d73fb3865
Matteo Sirri <matteosirri99gmail.com>
2022-09-07 22:40:02 +0200

    feat: add conf path for colab

--- configs/path_cfg.py
+++ configs/path_cfg.py
@@ -1,9 +1,14 @@
 import os
-MOTSYNTH_ROOT = os.getcwd() + '\storage\MOTSynth'
-MOTCHA_ROOT = os.getcwd() + '\storage\MOTChallenge'
-OUTPUT_DIR = os.getcwd() + '\storage\motsynth_output'
 
-# colab config
-# MOTSYNTH_ROOT = '/content/gdrive/MyDrive/CVCS/storage/user/brasoand/MOTSynth'
-# MOTCHA_ROOT = '/content/gdrive/MyDrive/CVCS/storage/slurm/brasoand/mpntrack'
-# OUTPUT_DIR = '/content/gdrive/MyDrive/CVCS/storage/slurm/brasoand/motsynth_output'
+cwd = os.getcwd()
+
+if(cwd == '/content'):
+    # colab config
+    MOTSYNTH_ROOT = cwd + '/gdrive/MyDrive/CVCS/storag/MOTSynth'
+    MOTCHA_ROOT = cwd + '/gdrive/MyDrive/CVCS/storage/MOTChallenge'
+    OUTPUT_DIR = cwd + '/gdrive/MyDrive/CVCS/storage/motsynth_output'
+else:
+    # windows config
+    MOTSYNTH_ROOT = cwd + '\storage\MOTSynth'
+    MOTCHA_ROOT = cwd + '\storage\MOTChallenge'
+    OUTPUT_DIR = cwd + '\storage\motsynth_output'

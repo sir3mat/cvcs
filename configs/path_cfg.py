b76196c6e58dfa64d78ad64932a1454a1e658755
Matteo Sirri <matteosirri99gmail.com>
2022-09-07 15:08:22 +0200

    build: add path for conda configs

--- configs/path_cfg.py
+++ configs/path_cfg.py
@@ -2,3 +2,8 @@ import os
 MOTSYNTH_ROOT = os.getcwd() + '\storage\MOTSynth'
 MOTCHA_ROOT = os.getcwd() + '\storage\MOTChallenge'
 OUTPUT_DIR = os.getcwd() + '\storage\motsynth_output'
+
+# colab config
+# MOTSYNTH_ROOT = '/content/gdrive/MyDrive/CVCS/storage/user/brasoand/MOTSynth'
+# MOTCHA_ROOT = '/content/gdrive/MyDrive/CVCS/storage/slurm/brasoand/mpntrack'
+# OUTPUT_DIR = '/content/gdrive/MyDrive/CVCS/storage/slurm/brasoand/motsynth_output'

afa770ca9e4be7bf872d68611b70b7e7949e3a3c
Matteo Sirri <matteosirri99gmail.com>
2022-10-19 10:43:49 +0200

    feat: add mot17 support, refactoring and show img after evaluate

--- tools/train_detector.py
+++ tools/train_detector.py
@@ -7,11 +7,9 @@ import coloredlogs
 import logging
 import torch
 import torch.utils.data
-
-from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn_v2, FasterRCNN, FastRCNNPredictor
-from torchvision.models.detection import FasterRCNN_ResNet50_FPN_V2_Weights
-from torchvision.models.resnet import ResNet50_Weights
-from src.detection.utils import save_train_loss_plot
+from src.detection.vision.mot_data import MOTObjDetect
+from src.detection.model_factory import ModelFactory
+from src.detection.utils import save_train_loss_plot, show_bbox
 
 import src.detection.vision.presets as presets
 import src.detection.vision.utils as utils
@@ -19,8 +17,8 @@ from src.detection.vision.engine import train_one_epoch, evaluate
 from src.detection.vision.group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
 from src.detection.mot_dataset import get_mot_dataset
 
-logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG')
+logger = logging.getLogger(__name__)
 
 
 def get_args_parser(add_help=True):
@@ -45,18 +43,18 @@ def get_args_parser(add_help=True):
 
     # training param
     parser.add_argument(
-        "-b", "--batch-size", default=2, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
+        "-b", "--batch-size", default=1, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
     )
-    parser.add_argument("--epochs", default=30, type=int,
+    parser.add_argument("--epochs", default=26, type=int,
                         metavar="N", help="number of total epochs to run")
     parser.add_argument(
         "-j", "--workers", default=0, type=int, metavar="N", help="number of data loading workers (default: 0)"
     )
     parser.add_argument(
         "--lr",
-        default=0.005,
+        default=0.0025,
         type=float,
-        help="initial learning rate, 0.005 is the default value",
+        help="initial learning rate, 0.0025 is the default value",
     )
     parser.add_argument(
         "--lr-scheduler", default="multisteplr", type=str, help="name of lr scheduler (default: multisteplr)"
@@ -129,7 +127,7 @@ def get_args_parser(add_help=True):
     parser.add_argument("--output-dir", default='fasterrcnn_training',
                         type=str, help="path to save outputs")
 
-    parser.add_argument("--aspect-ratio-group-factor", default=-1, type=int)
+    parser.add_argument("--aspect-ratio-group-factor", default=3, type=int)
     parser.add_argument("--rpn-score-thresh", default=None,
                         type=float, help="rpn score threshold for faster-rcnn")
 
@@ -163,10 +161,9 @@ def get_dataset(name, data_path, transform):
         raise ValueError(name)
 
     logger.debug(
-        f"get_dataset -> ann_file: {ann_file}")
+        f"get_dataset -> ann_file: {ann_file}, data_path: {data_path}")
 
     ds = get_mot_dataset(data_path, ann_file, transforms=transform)
-
     return ds, 2
 
 
@@ -178,38 +175,80 @@ def create_dataset(name: str, data_path: str, transform):
     return dataset, num_classes
 
 
+def create_data_loaders(dataset_train, dataset_train_no_random, dataset_test, args):
+    # random sampling on training dataset
+    train_sampler = torch.utils.data.RandomSampler(dataset_train)
+    # sequential sampling on test dataset
+    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    if args.aspect_ratio_group_factor >= 0:
+        group_ids = create_aspect_ratio_groups(
+            dataset_train, k=args.aspect_ratio_group_factor)
+        train_batch_sampler = GroupedBatchSampler(
+            train_sampler, group_ids, args.batch_size)
+    else:
+        train_batch_sampler = torch.utils.data.BatchSampler(
+            train_sampler, args.batch_size, drop_last=True)
+
+    # create train data loader
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset_train, batch_sampler=train_batch_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
+    )
+
+    data_loader_train_no_random = torch.utils.data.DataLoader(
+        dataset_train_no_random, batch_size=1, sampler=test_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
+    )
+    # create test data loader
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, sampler=test_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
+    )
+    return data_loader_train, data_loader_train_no_random, data_loader_test
+
+
+def create_optimizer(model, lr, momentum, weight_decay):
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(
+        params, lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+    # Total parameters and trainable parameters.
+    total_params = sum(p.numel() for p in model.parameters())
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    logger.debug(
+        f"Total trainable params: {total_trainable_params:}, Total params: {total_params}, lr: {lr}, momentum: {momentum}, weight_decay:{weight_decay}")
+
+    return optimizer
+
+
+def create_lr_scheduler(optimizer, args):
+    lr_scheduler_type = args.lr_scheduler.lower()
+    if lr_scheduler_type == "multisteplr":
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=args.lr_steps, gamma=args.lr_gamma)
+        logger.debug(
+            f"lr_scheduler: {lr_scheduler_type}, milestones: {args.lr_steps}, gamma: {args.lr_gamma}")
+
+    elif lr_scheduler_type == "cosineannealinglr":
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.epochs)
+        logger.debug(
+            f"lr_scheduler: {lr_scheduler_type}, T_max: {args.epochs}")
+    else:
+        raise RuntimeError(
+            f"Invalid lr scheduler '{lr_scheduler_type}'. Only MultiStepLR and CosineAnnealingLR are supported."
+        )
+    return lr_scheduler
+
+
 def resume_training(model, optimizer, lr_scheduler, scaler, args):
     checkpoint = torch.load(args.resume, map_location="cpu")
+
     model.load_state_dict(checkpoint["model"])
     optimizer.load_state_dict(checkpoint["optimizer"])
     lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
     args.start_epoch = checkpoint["epoch"] + 1
     if args.amp:
         scaler.load_state_dict(checkpoint["scaler"])
-
-
-class ModelFactory:
-    @staticmethod
-    def get_model(args):
-        if args.model == "fasterrcnn_resnet50_fpn":
-            backbone_name = "resnet50"
-            trainable_backbone_layers = 5
-            weights = FasterRCNN_ResNet50_FPN_V2_Weights.COCO_V1
-            # added for baseline
-            backbone_weights = ResNet50_Weights.IMAGENET1K_V2
-            model: FasterRCNN = fasterrcnn_resnet50_fpn_v2(weights=weights, backbone_name=backbone_name, weights_backbone=backbone_weights,
-                                                           trainable_backbone_layers=trainable_backbone_layers)
-            num_classes = 2  # 1 class (person) + background
-            # get number of input features for the classifier
-            in_features = model.roi_heads.box_predictor.cls_score.in_features
-            # replace the pre-trained head with a new one
-            model.roi_heads.box_predictor = FastRCNNPredictor(
-                in_features, num_classes)
-        else:
-            logger.error(
-                "Please, provide a valid model as argument. Select one of the following: fasterrcnn.")
-            raise ValueError(args.model)
-        return model
 
 
 def save_model_checkpoint(model, optimizer, lr_scheduler, epoch, scaler, args):
@@ -229,121 +268,125 @@ def save_model_checkpoint(model, optimizer, lr_scheduler, epoch, scaler, args):
             args.output_dir, "checkpoint.pth"))
 
 
-def main(args):
-    # create ouput_dir
-    if args.output_dir:
-        args.output_dir = osp.join(
-            OUTPUT_DIR, 'detection_logs', args.output_dir)
-        utils.mkdir(args.output_dir)
+def save_plot(losses_dict, batch_loss_dict):
+    if not losses_dict:
+        for name, metric in batch_loss_dict.items():
+            losses_dict[name] = []
+        for name, metric in batch_loss_dict.items():
+            losses_dict[name].extend(metric)
+        save_train_loss_plot(losses_dict, OUTPUT_DIR)
 
-    logger.info("COMMAND LINE ARGUMENTS")
-    logger.info(args)
+
+def main(args):
+    output_dir = None
+    if args.output_dir:
+        output_dir = osp.join(
+            OUTPUT_DIR, 'detection_logs', args.output_dir)
+        utils.mkdir(output_dir)
+
+    logger.debug("COMMAND LINE ARGUMENTS")
+    logger.debug(args)
 
     device = torch.device(args.device)
+    logger.debug(f"DEVICE: {device}")
 
-    logger.debug("CREATING DATASETS")
-    dataset_train, num_classes = create_dataset(
-        args.train_dataset, MOTSYNTH_ROOT, get_transform(True, args))
-    dataset_test, _ = create_dataset(
-        args.val_dataset, MOTCHA_ROOT, get_transform(False, args))
-    logger.debug(
-        f"train_dataset:{dataset_train}, num_classes:{num_classes}, validation_dataset:{dataset_test}")
+    logger.debug("CREATE DATASETS")
+    dataset_train = dataset_train_no_random = dataset_test = None
 
-    logger.debug("CREATING DATA LOADERS")
-    # random sampling on training dataset
-    train_sampler = torch.utils.data.RandomSampler(dataset_train)
-    # sequential sampling on test dataset
-    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+    if args.train_dataset == "motsynth":
+        dataset_train, _ = create_dataset(
+            args.train_dataset, MOTSYNTH_ROOT, get_transform(True, args))
+        dataset_train_no_random, _ = create_dataset(
+            args.train_dataset, MOTSYNTH_ROOT, get_transform(False, args))
+        dataset_test, _ = create_dataset(
+            args.val_dataset, MOTCHA_ROOT, get_transform(False, args))
 
-    if args.aspect_ratio_group_factor >= 0:
-        group_ids = create_aspect_ratio_groups(
-            dataset_train, k=args.aspect_ratio_group_factor)
-        train_batch_sampler = GroupedBatchSampler(
-            train_sampler, group_ids, args.batch_size)
-    else:
-        train_batch_sampler = torch.utils.data.BatchSampler(
-            train_sampler, args.batch_size, drop_last=True)
+    elif args.train_dataset == "mot17":
+        train_split_seqs = ['MOT17-02', 'MOT17-04', 'MOT17-05',
+                            'MOT17-09', 'MOT17-10', 'MOT17-11', 'MOT17-13']
+        test_split_seqs = ['MOT17-09']
+        for seq in test_split_seqs:
+            train_split_seqs.remove(seq)
+        dataset_train = MOTObjDetect(osp.join(MOTCHA_ROOT, "MOT17", 'train'), get_transform(
+            True, args), split_seqs=train_split_seqs)
+        dataset_train_no_random = MOTObjDetect(osp.join(
+            MOTCHA_ROOT, 'train'), get_transform(False, args), split_seqs=train_split_seqs)
+        dataset_test, _ = MOTObjDetect(osp.join(MOTCHA_ROOT, "MOT17", 'train'), get_transform(
+            False, args), split_seqs=test_split_seqs)
 
-    # create train data loader
-    data_loader_train = torch.utils.data.DataLoader(
-        dataset_train, batch_sampler=train_batch_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
-    )
-    # create test data loader
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=1, sampler=test_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
-    )
+    logger.debug("CREATE DATA LOADERS")
+    data_loader_train, data_loader_train_no_random, data_loader_test = create_data_loaders(
+        dataset_train, dataset_train_no_random, dataset_test, args)
 
-    logger.debug("CREATING MODEL")
-    logger.debug(f"MODEL: {args.model}")
+    logger.debug("CREATE MODEL")
     model = ModelFactory.get_model(args)
     model.to(device)
 
-    # perform only test
     if args.test_only:
+        logger.debug("TEST ONLY")
         evaluate(model, data_loader_test, device=device, iou_types=['bbox'])
+        # after evaluating
+        # pick one image from the test set and evaluate it to show bbox
+        for imgs, target in data_loader_train_no_random:
+            with torch.no_grad():
+                prediction = model([imgs[0].to(device)])[0]
+            show_bbox(imgs[0], prediction, 0.75)
+            show_bbox(imgs[0], target[0]['boxes'])
+            break
         return
 
-    # construct an optimizer
-    params = [p for p in model.parameters() if p.requires_grad]
-    # Total parameters and trainable parameters.
-    total_params = sum(p.numel() for p in model.parameters())
-    total_trainable_params = sum(
-        p.numel() for p in model.parameters() if p.requires_grad)
-    logger.debug(
-        f"training params: {total_trainable_params:}, total params: {total_params}")
+    logger.debug("CREATE OPTIMIZER")
+    optimizer = create_optimizer(
+        model, args.lr, args.momentum, args.weight_decay)
 
-    optimizer = torch.optim.SGD(
-        params, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    logger.debug("CREATE LR SCHEDULER")
+    lr_scheduler = create_lr_scheduler(optimizer, args)
 
-    # construct a learning rate scheduler
-    args.lr_scheduler = args.lr_scheduler.lower()
-    if args.lr_scheduler == "multisteplr":
-        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-            optimizer, milestones=args.lr_steps, gamma=args.lr_gamma)
-    elif args.lr_scheduler == "cosineannealinglr":
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=args.epochs)
-    else:
-        raise RuntimeError(
-            f"Invalid lr scheduler '{args.lr_scheduler}'. Only MultiStepLR and CosineAnnealingLR are supported."
-        )
-
-    # configure scaler for automatic mixed precision training (amp)
+    logger.debug("CONFIGURE SCALER FOR amp")
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
-    # resume training
     if args.resume:
+        logger.debug("RESUME TRAINING")
         resume_training(model, optimizer, lr_scheduler,
                         scaler, args)
 
     logger.debug("START TRAINING")
-
     losses_dict = {}
-
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
         _, batch_loss_dict = train_one_epoch(model, optimizer, data_loader_train, device,
                                              epoch, args.print_freq, scaler)
         lr_scheduler.step()
-        evaluate(model, data_loader_test,
-                 device=device, iou_types=['bbox'])
-
         # save train loss plot
-        if not losses_dict:
-            for name, metric in batch_loss_dict.items():
-                losses_dict[name] = []
-        for name, metric in batch_loss_dict.items():
-            losses_dict[name].extend(metric)
-        save_train_loss_plot(losses_dict, OUTPUT_DIR)
+        save_plot(losses_dict, batch_loss_dict)
 
         # save model
-        if (epoch % 2 == 0):
+        if epoch % 2 == 0:
+            evaluate(model, data_loader_test,
+                              device=device, iou_types=['bbox'])
             save_model_checkpoint(
                 model, optimizer, lr_scheduler, epoch, scaler, args)
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     logger.debug(f"TRAINING TIME: {total_time_str}")
+
+    # after training
+    # pick one image from the test set and evaluate it to show bbox
+    data_loader = torch.utils.data.DataLoader(
+        dataset_train_no_random, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    for imgs, target in data_loader:
+        print(dataset_train._img_paths[0])
+
+        model.eval()
+        with torch.no_grad():
+            prediction = model([imgs[0].to(device)])[0]
+
+        show_bbox(imgs[0], prediction['boxes'])
+        show_bbox(imgs[0], target[0]['boxes'])
+        break
 
 
 if __name__ == "__main__":

7f2ee2787dabff5d879701a83cf14601adc233dc
Matteo Sirri <matteosirri99gmail.com>
2022-10-24 14:27:33 +0200

    build: add gradio

--- app.py
+++ app.py
@@ -1,58 +1,63 @@
 import os.path as osp
+from tkinter.ttk import Style
 import gradio as gr
 import torch
 import logging
-from torchvision.utils import draw_bounding_boxes
 import torchvision
 from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn_v2
 from torchvision.utils import draw_bounding_boxes
-
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-
 from configs.path_cfg import MOTCHA_ROOT, OUTPUT_DIR
+from src.detection.graph_utils import add_bbox
 from src.detection.vision import presets
 logging.getLogger('PIL').setLevel(logging.CRITICAL)
 
 
-def add_bbox(img, output, th=None):
-    img_to_show = torch.clip(img*255, 0, 255)
-    img_to_show = img_to_show.type(torch.uint8)
-    img_with_bbbox = None
-    if th == None:
-        img_with_bbbox = draw_bounding_boxes(
-            img_to_show, boxes=output, width=4)
+def load_model(baseline: bool = False):
+    if baseline:
+        model = fasterrcnn_resnet50_fpn_v2(
+            weights="DEFAULT", weights_backbone="DEFAULT", trainable_backbone_layers=0)
     else:
-        img_with_bbbox = draw_bounding_boxes(
-            img_to_show, boxes=output["boxes"][output['scores'] > th], width=4)
-    return img_with_bbbox
-
-
-def load_model():
-    model = fasterrcnn_resnet50_fpn_v2()
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
-    checkpoint = torch.load(osp.join(OUTPUT_DIR, "detection_logs",
-                            "fasterrcnn_training", "checkpoint.pth"), map_location="cpu")
-    model.load_state_dict(checkpoint["model"])
+        model = fasterrcnn_resnet50_fpn_v2()
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
+        checkpoint = torch.load(osp.join(OUTPUT_DIR, "detection_logs",
+                                "fasterrcnn_training", "checkpoint.pth"), map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
     model = model.eval()
-    model.to("cuda")
     return model
 
 
-def run_inference(image):
+def detect_with_resnet50Model_finetuning_motsynth(image):
     model = load_model()
     transformEval = presets.DetectionPresetEval()
-    image_tensor = transformEval(image, None)
-    print(image_tensor)
-    prediction = model(image_tensor)[0]
-    image_w_bbox = add_bbox(image, prediction, 0.75)
-    torchvision.io.write_png(image_w_bbox, "out.png")
-    return "out.png"
+    image_tensor = transformEval(image, None)[0]
+    prediction = model([image_tensor])[0]
+    image_w_bbox = add_bbox(image_tensor, prediction, 0.75)
+    torchvision.io.write_png(image_w_bbox, "custom_out.png")
+    return "custom_out.png"
+
+
+def detect_with_resnet50Model_baseline(image):
+    model = load_model(baseline=True)
+    transformEval = presets.DetectionPresetEval()
+    image_tensor = transformEval(image, None)[0]
+    prediction = model([image_tensor])[0]
+    image_w_bbox = add_bbox(image_tensor, prediction, 0.75)
+    torchvision.io.write_png(image_w_bbox, "baseline_out.png")
+    return "baseline_out.png"
 
 
 title = "Performance comparision of Faster R-CNN for people detection with syntetic data"
-description = "Performance comparision of Faster R-CNN models for people detecion using MOTSynth and MOT17"
+description = "<p style='text-align: center'>Performance comparision of Faster R-CNN models for people detecion using MOTSynth and MOT17"
 examples = [[osp.join(MOTCHA_ROOT, "MOT17", "train",
                       "MOT17-09-DPM", "img1", "000001.jpg")]]
-gr.Interface(run_inference, gr.Image(type="pil"), gr.Image(
-    type="file"), title=title, description=description, examples=examples).launch(enable_queue=True)
+
+io_custom = gr.Interface(detect_with_resnet50Model_finetuning_motsynth, gr.Image(type="pil"), gr.Image(
+    type="file", shape=(1920, 1080), label="FasterR-CNN_Resnet50_FinteTuning_MOTSyth"))
+
+io_baseline = gr.Interface(detect_with_resnet50Model_baseline, gr.Image(type="pil"), gr.Image(
+    type="file", shape=(1920, 1080), label="FasterR-CNN_Resnet50_COCO"))
+
+gr.Parallel(io_baseline, io_custom, title=title,
+            description=description, examples=examples).launch(enable_queue=True)

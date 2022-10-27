dace5137090707eea658c8e3bb457591576d8a28
Matteo Sirri <matteosirri99gmail.com>
2022-10-27 15:41:48 +0200

    fix: fix typo

--- src/detection/vision/coco_eval.py
+++ src/detection/vision/coco_eval.py
@@ -55,8 +55,6 @@ class CocoEvaluator:
     def summarize(self):
         for iou_type, coco_eval in self.coco_eval.items():
             print(f"IoU metric: {iou_type}")
-            results = coco_eval.summarize()
-            return results
 
     def prepare(self, predictions, iou_type):
         if iou_type == "bbox":

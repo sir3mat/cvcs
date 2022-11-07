0aa746c9abfcc36bccac53c0fd98cd3beab2e0c9
Matteo Sirri <matteosirri99gmail.com>
2022-11-07 10:14:55 +0100

    feat: add model eval path for test only

--- scripts/evaluate_detector.sh
+++ scripts/evaluate_detector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -m  tools.train_detector --test-only
+python -m  tools.train_detector --model-eval "d://cvcspw/storage/motsynth_output/detection_logs/fasterrcnn_training/checkpoint.pth" --test-only

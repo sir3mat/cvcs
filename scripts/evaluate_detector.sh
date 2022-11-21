13b13dbd8366d961e7b31fbe9b7f71ceac1194ff
Matteo Sirri <matteosirri99@gmail.com>
2022-11-21 16:28:18 +0100

    fix: fix typo

--- scripts/evaluate_detector.sh
+++ scripts/evaluate_detector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -m  tools.experiments.evaluate_detector --model-eval "C:\Users\Matteo\Desktop\cvcs\storage\motsynth_output\detection_logs\frcnn_v1_finetuning_imagenet_motsynth_split3_MOT17_10epoche\checkpoint.pth" 
+python -m  tools.experiments.evaluate_detector --model-eval "path-to-model.pth" 

c6dcab2e02029f798a2f56b3fdbf166439407819
ilariamanghi <ilariamanghi98@gmail.com>
2023-05-25 10:25:16 +0200

    add scripts for reid with sift and color hist

---
+++ scripts/reid_cv/compute_sift.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python experiments.reid_cv.reid_sift.retrieval_dataset_preparation.compute_sift_descriptor --ann-path ./storage/MOTSynth/comb_annotations/motsynth_split3.json --imgs-dir ./storage/MOTSynth/reid
+
